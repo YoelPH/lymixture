10c25ea1ef8eeaa0167770b3c63be66f9b95d560
YoelPH <yoelhaas@gmail.com>
2024-10-04 21:28:24 +0200

    change: Commented out sampling sampling introduced a circular import with lyscripts

--- lymixture/utils.py
+++ lymixture/utils.py
@@ -13,7 +13,7 @@ import lymph
 import numpy as np
 import pandas as pd
 import scipy as sp
-from lyscripts.sample import DummyPool, run_mcmc_with_burnin
+# from lyscripts.sample import DummyPool, run_mcmc_with_burnin #this creates a circular import. I should not import lyscripts here!
 from scipy.special import factorial
 
 warnings.filterwarnings("ignore", category=pd.errors.PerformanceWarning)
@@ -296,78 +296,78 @@ def emcee_sampling(llh_function, n_params, sample_name, llh_args=None):
     return samples
 
 
-def emcee_sampling_ext(
-    llh_function,
-    n_params=None,
-    sample_name=None,
-    n_burnin=None,
-    n_step=None,
-    start_with=None,
-    llh_args=None,
-):
-    nwalkers = 20 * n_params
-    burnin = 1000 if n_burnin is None else n_burnin
-    nstep = 1000 if n_step is None else n_step
-    thin_by = 1
-    logger.info(f"Dimension: {n_params} with n walkers: {nwalkers}")
-    output_name = sample_name
+# def emcee_sampling_ext(
+#     llh_function,
+#     n_params=None,
+#     sample_name=None,
+#     n_burnin=None,
+#     n_step=None,
+#     start_with=None,
+#     llh_args=None,
+# ):
+#     nwalkers = 20 * n_params
+#     burnin = 1000 if n_burnin is None else n_burnin
+#     nstep = 1000 if n_step is None else n_step
+#     thin_by = 1
+#     logger.info(f"Dimension: {n_params} with n walkers: {nwalkers}")
+#     output_name = sample_name
 
-    created_pool = DummyPool()
-    with created_pool as pool:
-        if start_with is None:
-            starting_points = np.random.uniform(size=(nwalkers, n_params))
-        else:
-            if np.shape(start_with) != np.shape(
-                np.random.uniform(size=(nwalkers, n_params))
-            ):
-                starting_points = np.tile(start_with, (nwalkers, 1))
-            else:
-                starting_points = start_with
-        logger.info(
-            f"Start Burning (steps = {burnin}) with {created_pool._processes} cores"
-        )
-        burnin_sampler = emcee.EnsembleSampler(
-            nwalkers,
-            n_params,
-            llh_function,
-            args=llh_args,
-            pool=pool,
-        )
-        burnin_results = burnin_sampler.run_mcmc(
-            initial_state=starting_points, nsteps=burnin, progress=True
-        )
+#     created_pool = DummyPool()
+#     with created_pool as pool:
+#         if start_with is None:
+#             starting_points = np.random.uniform(size=(nwalkers, n_params))
+#         else:
+#             if np.shape(start_with) != np.shape(
+#                 np.random.uniform(size=(nwalkers, n_params))
+#             ):
+#                 starting_points = np.tile(start_with, (nwalkers, 1))
+#             else:
+#                 starting_points = start_with
+#         logger.info(
+#             f"Start Burning (steps = {burnin}) with {created_pool._processes} cores"
+#         )
+#         burnin_sampler = emcee.EnsembleSampler(
+#             nwalkers,
+#             n_params,
+#             llh_function,
+#             args=llh_args,
+#             pool=pool,
+#         )
+#         burnin_results = burnin_sampler.run_mcmc(
+#             initial_state=starting_points, nsteps=burnin, progress=True
+#         )
 
-        ar = np.mean(burnin_sampler.acceptance_fraction)
-        logger.info(
-            f"the HMM sampler for model 01 accepted {ar * 100 :.2f} % of samples."
-        )
-        starting_points = burnin_sampler.get_last_sample()[0]
-        # logger.info(f"The shape of the last sample is {starting_points.shape}")
-        original_sampler_mp = emcee.EnsembleSampler(
-            nwalkers,
-            n_params,
-            llh_function,
-            args=llh_args,
-            backend=None,
-            pool=pool,
-        )
-        sampling_results = original_sampler_mp.run_mcmc(
-            initial_state=starting_points,
-            nsteps=nstep,
-            progress=True,
-            thin_by=thin_by,
-        )
+#         ar = np.mean(burnin_sampler.acceptance_fraction)
+#         logger.info(
+#             f"the HMM sampler for model 01 accepted {ar * 100 :.2f} % of samples."
+#         )
+#         starting_points = burnin_sampler.get_last_sample()[0]
+#         # logger.info(f"The shape of the last sample is {starting_points.shape}")
+#         original_sampler_mp = emcee.EnsembleSampler(
+#             nwalkers,
+#             n_params,
+#             llh_function,
+#             args=llh_args,
+#             backend=None,
+#             pool=pool,
+#         )
+#         sampling_results = original_sampler_mp.run_mcmc(
+#             initial_state=starting_points,
+#             nsteps=nstep,
+#             progress=True,
+#             thin_by=thin_by,
+#         )
 
-        ar = np.mean(original_sampler_mp.acceptance_fraction)
-        logger.info(f"the HMM sampler for model accepted {ar * 100 :.2f} % of samples.")
-        samples = original_sampler_mp.get_chain(flat=True)
-        log_probs = original_sampler_mp.get_log_prob(flat=True)
-        end_point = original_sampler_mp.get_last_sample()[0]
-        if output_name is not None:
-            np.save(f"./samples/" + output_name, samples)
-        # plots["acor_times"].append(burnin_info["acor_times"][-1])
-        # plots["accept_rates"].append(burnin_info["accept_rates"][-1])
-    return samples, end_point, log_probs
+#         ar = np.mean(original_sampler_mp.acceptance_fraction)
+#         logger.info(f"the HMM sampler for model accepted {ar * 100 :.2f} % of samples.")
+#         samples = original_sampler_mp.get_chain(flat=True)
+#         log_probs = original_sampler_mp.get_log_prob(flat=True)
+#         end_point = original_sampler_mp.get_last_sample()[0]
+#         if output_name is not None:
+#             np.save(f"./samples/" + output_name, samples)
+#         # plots["acor_times"].append(burnin_info["acor_times"][-1])
+#         # plots["accept_rates"].append(burnin_info["accept_rates"][-1])
+#     return samples, end_point, log_probs
 
 
 def convert_lnl_to_filename(lnls):
